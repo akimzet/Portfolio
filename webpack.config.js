61f796df5a0c38e1ff9af7f087650827f4beb700
Andrew Kim <akimzet@gmail.com>
2019-09-11 14:12:08 -0700

    Finished basic webpack setup/linting.

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+/* eslint-disable */
+const path = require('path');
+const webpack = require('webpack');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '/dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+      }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+                exclude: [/node_modules/],
+            },
+        ],
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/,
+                },
+            },
+
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true,
+        },
+    },
+
+    devServer: {
+        port: "9000",
+        open: true,
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
